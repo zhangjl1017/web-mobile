4a42e8574e9a518cf0b31b85a82982e212dbb9db
p_gleizhang <zhangjianlei@yuewen.com>
2019-02-26 19:32:47 +0800

    fix wss

--- main.d1cb3.js
+++ main.d1cb3.js
@@ -135,7 +135,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.c9d29.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.f0bf3.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -161,7 +161,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.dae5f.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.d1cb3.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
