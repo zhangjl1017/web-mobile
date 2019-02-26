c93314e18f7987bcdc2d4b4b942aea098b2eba8e
p_gleizhang <zhangjianlei@yuewen.com>
2019-02-26 11:18:56 +0800

    add

--- main.aa18f.js
+++ main.aa18f.js
@@ -135,7 +135,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.824fe.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.0d852.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -161,7 +161,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.44560.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.aa18f.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
